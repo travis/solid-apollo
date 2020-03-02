dfeaf4ddda147fdbdb10fe3afd3cb1f2d697656b
Travis Vachon <travis.vachon@gmail.com>
2020-03-02 15:33:35 -0800

    Clean up VSCode tabs
    Gah!!

--- src/SolidLink.tsx
+++ src/SolidLink.tsx
@@ -1,5 +1,5 @@
 import { ApolloLink, Observable, Operation, FetchResult } from 'apollo-link';
-import {Client} from "graphql-ld";
+import { Client } from "graphql-ld";
 import { QueryEngineComunicaSolid } from "graphql-ld-comunica-solid";
 import { JsonLdContext } from "jsonld-context-parser";
 import { print } from 'graphql/language/printer';
@@ -8,18 +8,18 @@ export default class SolidLink extends ApolloLink {
   public client: Client;
   constructor(context: JsonLdContext, sources: string[]) {
     super();
-    this.client = new Client({ context, queryEngine: new QueryEngineComunicaSolid({sources}) });
+    this.client = new Client({ context, queryEngine: new QueryEngineComunicaSolid({ sources }) });
   }
   request(operation: Operation) {
-    const runQuery = this.client.query({query: print(operation.query)})
+    const runQuery = this.client.query({ query: print(operation.query) })
     return new Observable<FetchResult>(observer => {
-        runQuery.then(response => {
-          operation.setContext({ response })
-          observer.next(response)
-          observer.complete()
-        }).catch(err => {
-          observer.error(err)
-        })
+      runQuery.then(response => {
+        operation.setContext({ response })
+        observer.next(response)
+        observer.complete()
+      }).catch(err => {
+        observer.error(err)
+      })
     })
   }
 }
--- test/SolidLink.spec.tsx
+++ test/SolidLink.spec.tsx
@@ -3,14 +3,14 @@ import SolidLink from '../src/SolidLink'
 import gql from "graphql-tag";
 import { InMemoryCache } from 'apollo-cache-inmemory';
 describe('SolidLink', () => {
-it('should work with an ApolloClient to execute GraphQL queries', async () => {
+  it('should work with an ApolloClient to execute GraphQL queries', async () => {
     const context = {
       "@context": {
         "foaf": "http://xmlns.com/foaf/0.1/",
         "name": "foaf:name"
       }
     };
-    
+
     const query = gql`
   query @single(scope: all) {
     id
@@ -19,13 +19,13 @@ it('should work with an ApolloClient to execute GraphQL queries', async () => {
 
 
     const client = new ApolloClient({
-      cache: new InMemoryCache({addTypename: false}),
+      cache: new InMemoryCache({ addTypename: false }),
       link: new SolidLink(context, ["https://tvachon.inrupt.net/profile/card#me"])
     })
-    const {data} = await client.query({query})
+    const { data } = await client.query({ query })
     expect(data).toEqual({
       id: "https://tvachon.inrupt.net/profile/card#me",
       name: "Travis Vachon"
     })
-  })  
+  })
 })
